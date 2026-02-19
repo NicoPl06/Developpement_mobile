38ad53c827ea5588a306c44dd5130103ba8054c3
Nicolas Plaisance <nicolas.ar.plaisance@gmail.com>
2026-02-19 18:03:39 +0100

    Ajout night mode (bonus)

--- app/src/main/java/iut/dam/powerhome/SettingsFragment.java
+++ app/src/main/java/iut/dam/powerhome/SettingsFragment.java
@@ -1,15 +1,45 @@
 package iut.dam.powerhome;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup c, Bundle b) {
         return inflater.inflate(R.layout.fragment_settings, c, false);
+
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button nightBtn = view.findViewById(R.id.night);
+        Button boldBtn = view.findViewById(R.id.bold);
+
+        nightBtn.setOnClickListener(v -> {
+            int current = AppCompatDelegate.getDefaultNightMode();
+
+            if (current == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+
+            requireActivity().recreate();
+        });
+
+    }
+
 }
